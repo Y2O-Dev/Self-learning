78352e044dab229b287789c5743ec1624f5b92df
YAKUB_YO <yakuboyusuf@gmail.com>
2023-10-08 16:34:18 +0100

    modified aws-resource-usage.sh for better ux

--- Bash-Scripting/AWS_Bash_Sripting_Project/aws-resource-usage.sh
+++ Bash-Scripting/AWS_Bash_Sripting_Project/aws-resource-usage.sh
@@ -12,13 +12,17 @@
 # IAM Users
 
 # list EC2 instances
+echo "list all instances in the user account"
 aws-vault exec y2o -- aws ec2 describe-instances
 
 # list all the s3 buckets
+echo "list all s3 bucket in the user account"
 aws-vault exec y2o -- aws s3 ls
 
 # list lambda
+echo "list all instalambda functions in the user account"
 aws-vault exec y2o -- aws lambda list-functions
 
 # list IAM users
+echo "list all iam users in the user account"
 aws-vault exec y2o -- aws iam list-users
