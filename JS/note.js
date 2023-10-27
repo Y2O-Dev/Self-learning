980ddceabe5af2cfa267cc56a073d6e2489d1bef
YAKUB_YO <yakuboyusuf@gmail.com>
2023-10-27 06:23:25 +0100

    modify node file

--- JS/note.js
+++ JS/note.js
@@ -2,3 +2,39 @@ var x = "JavaScript ";
 var y = "in 8 Hours";
 var str = x + y;
 console.log(str);
+
+// template literals
+console.log(`Half of 100 is ${100 / 2}`)
+
+// Types of operators: binary & Unary
+console.log(typeof 4.5)
+
+// Boolean Values
+console.log(4 > 7)
+console.log(NaN == NaN)
+
+// Logical Operators
+console.log(4 && 7)
+
+// Bindings or Variables
+
+let caught = 5 * 5;
+console.log(caught)
+console.log(caught * caught)
+
+let state = "happy";
+console.log(state)
+state = "grumpy"
+console.log(state)
+
+var name = "Yakub"; // var was used pre-2015 JavaScript
+const greeting = "Hello! ";
+console.log(greeting + name)
+
+// Functions
+// let theNumber = Number(prompt("Pick a number"));
+// let ask = prompt("Enter passcode");
+
+// Return Values
+console.log(Math.max(6, 9))
+
