ffba4400e17b97a86748bab911701b196561fb0a
Y2O-Dev <yakuboyusuf@gmail.com>
2024-08-14 21:56:24 +0100

    nohup with rando emoji

--- Bash-Scripting/nohup.sh
+++ Bash-Scripting/nohup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-#Testing nohup command with infinite while loop
-var=0
-while [ $var > 0 ]; do
-    # Your commands go here
-    echo "$var This is an infinite loop. Press [CTRL+C] to stop."
-    sleep 1  # Pause for 1 second to make the output more readable
-    var=`expr $var + 1`
-done
+# #Testing nohup command with infinite while loop
+# var=0
+# while [ $var > 0 ]; do
+#     # Your commands go here
+#     echo "$var This is an infinite loop. Press [CTRL+C] to stop."
+#     sleep 1  # Pause for 1 second to make the output more readable
+#     var=`expr $var + 1`
+# done
+
+#!/usr/bin/env bash
+
+# while true; do
+# 	rand=$(shuf -i 2600-2700 -n 1)
+# 	echo -en "   \u$rand"
+# 	sleep 0
+# done
