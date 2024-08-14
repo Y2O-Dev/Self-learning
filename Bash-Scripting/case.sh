0598f112ec8124bd04acf609bcbdb1911876e42f
Y2O-Dev <yakuboyusuf@gmail.com>
2024-08-14 13:37:10 +0100

    Case with Extended Pattern Matching

--- Bash-Scripting/case.sh
+++ Bash-Scripting/case.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
+shopt -s extglob
 
-read -p "Enter Yes/No: " choice
+# read -p "Enter Yes/No: " choice
 
-case "$choice" in
+# case "$choice" in
 
-y|yes|Yes|YES|Y)
-        echo "You entered $choice as Yes"
-        ;;
-n|no|No|NO|N)
-        echo "You entered $choice as No"
-        ;;
+# y|yes|Yes|YES|Y)
+#         echo "You entered $choice as Yes"
+#         ;;
+# n|no|No|NO|N)
+#         echo "You entered $choice as No"
+#         ;;
+# *)
+#         echo "Enter a valid option"
+#         ;;
+# esac
+
+
+#!/bin/bash
+
+# read -p "Enter a number between 0 and 9: " REPLY
+
+# case "$REPLY" in
+#     0|1)
+#         echo "Your response is $REPLY and it's a binary number" ;;
+#     2|3|4|5|6|7|8|9)
+#         echo "Your response is $REPLY but it's not a binary number" ;;
+#     *)
+#         echo "$REPLY you entered is a wrong number, try again" ;;
+# esac
+
+
+read -p "Enter a number between 0 and 1:  "
+
+case "$REPLY" in
+
+0|1)
+   echo "Your respose is $REPLY and its a binary number" ;;
+
++([2-9]))
+   echo "Your respose is $REPLY but its not a binary number" ;;
+
 *)
-        echo "Enter a valid option"
-        ;;
+   echo "$REPLY you entered is a wrong number, try again" ;;
+
 esac
